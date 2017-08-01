7ff2ded797dcd4455ba9ca39a4c5e05f6ea75fb6
Wong Lok <yellowhappy831@gmail.com>
2017-08-01 16:35:04 +0800

    a hybrid can withstand these things lol

--- LokLokMetal/ViewController.swift
+++ LokLokMetal/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
