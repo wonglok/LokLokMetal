aea1e8402bc23657409ee7e2e30db747951b3531
Wong Lok <yellowhappy831@gmail.com>
2017-08-01 16:33:21 +0800

    Initial Commit

---
+++ LokLokMetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LokLokMetal
+//
+//  Created by LOK on 1/8/2017.
+//  Copyright © 2017 WONG LOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
